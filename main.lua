96abe0a7a32136d75c33a8114f79e543fa59aeb8
NakeleKantoo <zzleozz2017@gmail.com>
2024-07-23 14:33:33 -0300

    done

--- main.lua
+++ main.lua
@@ -4,35 +4,42 @@ require('utils.draw')
 
 math.randomseed(os.time(),os.time()-150)
 
-local ongame = false
-local hub = noobhub.new({server="187.73.30.41", port="15565"})
-local channel = math.random(10000,99999)
-local myTurn = true
-local inputchannel = ""
-local onmychannel = false
-local gameEnd,gameMessage = false, ""
+hub = noobhub.new({server="187.73.30.41", port="15565"})
+channel = math.random(10000,99999)
+myTurn = true
+inputchannel = ""
+gameMessage = ""
+menu = "start"
 
-local table = {}
+board = {}
 
-local redcolors = {}
+redcolors = {}
 redcolors[#redcolors+1] = {1, 0.851, 0}
 redcolors[#redcolors+1] = {1, 0.459, 0}
 redcolors[#redcolors+1] = {1, 0, 0.271}
 
-local bluecolors = {}
+bluecolors = {}
 bluecolors[#bluecolors+1] = {0.29, 1, 0.659}
 bluecolors[#bluecolors+1] = {0, 0.949, 1}
 bluecolors[#bluecolors+1] = {0.29, 0.42, 1}
 
-local font = 32
+font = 32
 
-local team = 1 --1 for red 2 for blue
-local squaresAvailable = {}
+team = 1 --1 for red 2 for blue
+squaresAvailable = {}
 squaresAvailable[#squaresAvailable+1] = 3
 squaresAvailable[#squaresAvailable+1] = 3
 squaresAvailable[#squaresAvailable+1] = 3
-local selectedSquare = 1
+selectedSquare = 1
 
+cursorBlink = 0.5
+cursorBlinkMax = 0.5
+cursorBlinkNow = false
+unable = false
+unableFn = function()end
+dummyBoard = {}
+dummyBoardReset = 5
+dummyBoardResetMax = 5
 
 local centerspacing = 100
 
@@ -49,144 +56,41 @@ end
 
 function love.update(dt)
     hub:enterFrame()
+    cursorBlink=cursorBlink-dt
+    if cursorBlink<=0 then
+        cursorBlink=cursorBlinkMax
+        cursorBlinkNow=not cursorBlinkNow
+    end
+    dummyBoardReset=dummyBoardReset-dt
+    if dummyBoardReset<=0 then
+        resetDummyBoard()
+        dummyBoardReset=dummyBoardResetMax
+    end
 end
 
 function love.draw()
-    if ongame and gameEnd==false then
-        x = screenw/2-(wspacing*3)/2+wspacing
-        y = 50
-        for i=1,2 do
-            love.graphics.setColor(1,1,1)
-            love.graphics.rectangle('fill',x,y,10,hspacing*3)
-            x=x+wspacing
-        end
-        x = screenw/2-(wspacing*3)/2
-        y = 50+hspacing
-        for i=1,2 do
-            love.graphics.setColor(1,1,1)
-            love.graphics.rectangle('fill',x,y,wspacing*3,10)
-            y=y+hspacing
-        end
-
-        for i=1,9 do --search for any obj
-            local obj = table[i]
-            bx = (i-1)%3+1 --block x
-            by = math.floor((i-1)/3)+1 --block y
-            if obj==1 then
-                local size = 25
-                x = screenw/2-(wspacing*3)/2+(wspacing*(bx-1))+(wspacing/2-size/2)+5
-                y = 50+(hspacing*(by-1))+(hspacing/2-size/2)+5
-                love.graphics.setColor(redcolors[1])
-                love.graphics.rectangle('fill',x,y,size,size)
-            elseif obj==2 then
-                local size = 25
-                x = screenw/2-(wspacing*3)/2+(wspacing*(bx-1))+(wspacing/2-size/2)+5
-                y = 50+(hspacing*(by-1))+(hspacing/2-size/2)+5
-                love.graphics.setColor(bluecolors[1])
-                love.graphics.rectangle('fill',x,y,size,size)
-            elseif obj==3 then
-                local size = 50
-                x = screenw/2-(wspacing*3)/2+(wspacing*(bx-1))+(wspacing/2-size/2)+5
-                y = 50+(hspacing*(by-1))+(hspacing/2-size/2)+5
-                love.graphics.setColor(redcolors[2])
-                love.graphics.rectangle('fill',x,y,size,size)
-            elseif obj==4 then
-                local size = 50
-                x = screenw/2-(wspacing*3)/2+(wspacing*(bx-1))+(wspacing/2-size/2)+5
-                y = 50+(hspacing*(by-1))+(hspacing/2-size/2)+5
-                love.graphics.setColor(bluecolors[2])
-                love.graphics.rectangle('fill',x,y,size,size)
-            elseif obj==5 then
-                local size = 75
-                x = screenw/2-(wspacing*3)/2+(wspacing*(bx-1))+(wspacing/2-size/2)+5
-                y = 50+(hspacing*(by-1))+(hspacing/2-size/2)+5
-                love.graphics.setColor(redcolors[3])
-                love.graphics.rectangle('fill',x,y,size,size)
-            elseif obj==6 then
-                local size = 75
-                x = screenw/2-(wspacing*3)/2+(wspacing*(bx-1))+(wspacing/2-size/2)+5
-                y = 50+(hspacing*(by-1))+(hspacing/2-size/2)+5
-                love.graphics.setColor(bluecolors[3])
-                love.graphics.rectangle('fill',x,y,size,size)
-            end
-        end
-
-        x=25
-        y=screenh-screenh/4+25
-
-        for i=1,3 do --draw squares
-            local size = 25*i
-            local spacing = 25*i+10
-            if team==1 then
-                love.graphics.setColor(redcolors[i])
-            elseif team==2 then
-                love.graphics.setColor(bluecolors[i])
-            end
-            love.graphics.rectangle('fill',x+(spacing/2-size/2),y+(75/2-size/2),size,size)
-            love.graphics.setColor(1,1,1)
-            if selectedSquare==i then
-                love.graphics.rectangle('line',x+(spacing/2-size/2),y+(75/2-size/2),size,size,5)
-            end
-            love.graphics.printf(squaresAvailable[i],font,x,y+75,spacing,"center")
-            x=x+spacing
-        end
-
-        love.graphics.setColor(1,1,1)
-        if myTurn then
-            love.graphics.printf("É seu turno!",font,0,y+15,screenw-screenw/15,"right")
-        else
-            love.graphics.printf("Aguarde!",font,0,y+15,screenw-screenw/15,"right")
-        end
-
-    elseif gameEnd==false then
-        y = 50
-        love.graphics.printf("GigaVelha",font,0,y,screenw,"center")
-        y=y+50
-        love.graphics.printf("Toque em qualquer lugar da tela para usar o teclado",font,0,y,screenw,"center")
-        y=screenh/2-font:getHeight()/2
-        if onmychannel==false then
-            love.graphics.printf("Digite o número da sala de alguém",font,0,y,screenw,"center")
-            local _, wrap = font:getWrap("Digite o número da sala de alguém",screenw)
-            y=y+(font:getHeight()*#wrap)
-            love.graphics.printf("Sala: "..inputchannel,font,0,y,screenw,"center")
-            y=screenh-font:getHeight()-45
-            love.graphics.setColor(0.1,0.1,0.1)
-            local tw,th = font:getWrap("Criar uma sala",screenw-30)
-            love.graphics.rectangle('fill',screenw/2-tw/2-15+10,y-15+10,tw+30,font:getHeight()*#th+30)
-            love.graphics.setColor(0.45,0.45,0.45)
-            love.graphics.rectangle('fill',screenw/2-tw/2-15,y-15,tw+30,font:getHeight()*#th+30)
-            love.graphics.setColor(1,1,1)
-            love.graphics.printf("Criar uma sala",font,15,y,screenw-30,"center")
-        else
-            love.graphics.printf("Sala: "..channel,font,0,y,screenw,"center")
-            y=y+font:getHeight()
-            love.graphics.printf("Esperando alguém entrar...",font,0,y,screenw,"center")
-        end
+    if menu=="game" then
+        drawGame()
+    elseif menu=="start" or menu=="waitingmyown" or menu=="waitingqueue" or menu=="typenumber" then
+        drawStartMenu()
     else
-        y = 50
-        love.graphics.printf("GigaVelha",font,0,y,screenw,"center")
-        y=screenh/2-font:getHeight()/2
-        love.graphics.printf(gameMessage,font,0,y,screenw,"center")
+        drawEndMenu()
     end
 end
 
 function loadTable()
     for i=1,9 do
-        table[i] = 0
+        board[i] = 0
     end
 end
 
 function love.mousepressed(mx,my)
-    if ongame==false then
-        menuButtonCollision(mx,my)
-        return false
-    end
-    if gameEnd then
+    if menu=="end" then
         resetVars()
         return false
     end
 
-    if myTurn==false then return false end
+    if myTurn==false or menu~="game" then return false end
 
     --bounds
     --x+w >= cx and x <= cx + cw and y+h >= cy and y <= cy + ch
@@ -196,8 +100,8 @@ function love.mousepressed(mx,my)
         if squaresAvailable[selectedSquare]<=0 then return false end
         boxNumber = tableBounds(mx,my)
         local squarePut = team+(2*selectedSquare-2)
-        if squarePut>=table[boxNumber]+team then
-            table[boxNumber] = squarePut
+        if squarePut>=board[boxNumber]+team then
+            board[boxNumber] = squarePut
             publish("play",json.encode({play=boxNumber,square=squarePut}))
             myTurn=false
             squaresAvailable[selectedSquare]=squaresAvailable[selectedSquare]-1 --take one
@@ -217,12 +121,8 @@ function love.mousepressed(mx,my)
     end
 end
 
-function menuButtonCollision(mx,my)
-    
-end
-
 function love.textinput(t)
-    if ongame==false and onmychannel==false then
+    if menu=="typenumber" then
         if tonumber(t) then
             inputchannel=inputchannel..t
             inputchannel=string.sub(inputchannel,1,5)
@@ -231,21 +131,23 @@ function love.textinput(t)
 end
 
 function love.keypressed(key)
-    if ongame==false then
-        if key=="backspace" and onmychannel==false then
+    if menu~="game" then
+        if key=="backspace" and menu=="typenumber" then
             inputchannel=string.sub(inputchannel,1,#inputchannel-1)
         end
-        if key=="n" then
-            onmychannel=true
-            enterGame(channel)
-        end
-        if key=="return" then
+        if key=="return" and menu=="typenumber" then
             enterGame(inputchannel)
-            ongame=true
+            menu="game"
             myTurn=false
             team=2
         end
     end
+end
+
+function love.mousereleased()
+    unable=false
+    unableFn()
+    unableFn=function()end
 end
 
 function tableBounds(mx,my)
@@ -271,11 +173,11 @@ function checkVictory()
         for i, combination in ipairs(winningCombinations) do
             local a, b, c = combination[1], combination[2], combination[3]
             if t==1 then
-                if isOdd(table[a]) and isOdd(table[b]) and isOdd(table[c]) and table[a]>0 and table[b]>0 and table[c]>0 then
+                if isOdd(board[a]) and isOdd(board[b]) and isOdd(board[c]) and board[a]>0 and board[b]>0 and board[c]>0 then
                    return t
                 end
             else
-                if not isOdd(table[a]) and not isOdd(table[b]) and not isOdd(table[c]) and table[a]>0 and table[b]>0 and table[c]>0 then
+                if not isOdd(board[a]) and not isOdd(board[b]) and not isOdd(board[c]) and board[a]>0 and board[b]>0 and board[c]>0 then
                     return t
                  end
             end
@@ -283,7 +185,7 @@ function checkVictory()
     end
 
     for i = 1, 9 do
-        if table[i]<=2 then
+        if board[i]<=2 then
             return nil -- Game is still ongoing
         end
     end
@@ -316,38 +218,70 @@ function enterGame(channel)
     hub:subscribe({
         channel = channel,
         callback = function(message)
+            print("RECEIVED: "..message.action)
             if message.action=="join" then
-                ongame=true
+                menu="game"
             elseif message.action=="play" then
                 local play = json.decode(message.content)
-                table[play.play]=play.square
+                board[play.play]=play.square
                 myTurn=true
                 winner = checkVictory()
                 if winner=="draw" then
                     publish("draw","")
-                    gameEnd=true
+                    menu="end"
                     gameMessage="Empate!"
-                    loadTable()
                 elseif winner==1 or winner==2 then
                     publish("win",winner)
-                    gameEnd=true
+                    menu="end"
                     if winner==team then
                         gameMessage="Você venceu!"
                     else
                         gameMessage="Você perdeu! :("
                     end
-                    loadTable()
                 end
             elseif message.action=="draw" then
-                gameEnd=true
+                menu="end"
                 gameMessage="Empate!"
             elseif message.action=="win" then
-                gameEnd=true
-                print(team, message.content)
+                menu="end"
                 if message.content==team then
                     gameMessage="Você venceu!"
                 else
                     gameMessage="Você perdeu! :("
+                end
+            elseif message.action=="rematch" then
+                menu="rematch"
+            elseif message.action=="rematchaccept" then
+                menu="game"
+                resetVars()
+                myTurn=false
+                team=2
+            end
+        end
+    })
+    publish("join","")
+end
+
+function enterQueue(channel)
+    hub:subscribe({
+        channel = channel,
+        callback = function(message)
+            if message.action=="join" then
+                publish("game",channel)
+            end
+            if message.action=="game" then
+                publish("accept",message.content)
+                enterGame(message.content)
+                menu="game"
+                myTurn=false
+                team=2
+            end
+            if message.action=="accept" then
+                if message.content==channel then
+                    enterGame(channel)
+                    menu="game"
+                    myTurn=true
+                    team=1
                 end
             end
         end
@@ -369,10 +303,7 @@ function publish(action,content)
 end
 
 function resetVars()
-    ongame=false
     channel=math.random(10000,99999)
-    gameEnd=false
-    onmychannel=false
     inputchannel=""
     loadTable()
     myTurn=true
---
+++ utils/draw.lua
@@ -0,0 +1,322 @@
+function drawStartMenu()
+    y = 50
+    love.graphics.setColor(0.1,0.1,0.1)
+    love.graphics.printf("GigaVelha",font,5,y+5,screenw,"center")
+    love.graphics.setColor(1,1,1)
+    love.graphics.printf("GigaVelha",font,0,y,screenw,"center")
+    y=screenh/2-font:getHeight()/2
+    if menu=="start" then
+        newDummyBoard()
+        y=screenh-font:getHeight()-45
+        btnWrap("Entrar na fila",y,function ()
+            menu="waitingqueue"
+            enterQueue(0)
+        end)
+        y=y-font:getHeight()-45
+        btnWrap("Criar uma sala",y,function ()
+            menu="waitingmyown"
+            enterGame(channel)
+        end)
+        y=y-font:getHeight()-45
+        btnWrap("Entrar em uma sala",y,function ()
+            menu="typenumber"
+        end)
+        if (hspacing*3+20)<y-(50+font:getHeight()+15) then drawBoard(50+font:getHeight()+15) end
+    elseif menu=="typenumber" then
+        y=y-font:getHeight()-30
+        love.graphics.printf("Digite o número da sala de alguém",font,0,y,screenw,"center")
+        local _, wrap = font:getWrap("Digite o número da sala de alguém",screenw)
+        y=y+(font:getHeight()*#wrap)+30
+        textboxWrapper("Sala:",inputchannel,y)
+        y=y+font:getHeight()+45
+        btnWrap("Entrar",y,function ()
+            menu="game"
+            enterGame(inputchannel)
+            team=2
+            myTurn=false
+        end)
+        y=screenh-font:getHeight()-45
+        btnWrap("Voltar",y,function ()
+            menu="start"
+            hub:unsubscribe()
+        end)
+    elseif menu=="waitingmyown" then
+        love.graphics.printf("Sala: "..channel,font,0,y,screenw,"center")
+        y=y+font:getHeight()
+        love.graphics.printf("Esperando alguém entrar...",font,0,y,screenw,"center")
+        y=screenh-font:getHeight()-45
+        btnWrap("Voltar",y,function ()
+            menu="start"
+            hub:unsubscribe()
+        end)
+    elseif menu=="waitingqueue" then
+        love.graphics.printf("Aguardando na fila!",font,0,y,screenw,"center")
+        y=screenh-font:getHeight()-45
+        btnWrap("Voltar",y,function ()
+            menu="start"
+            hub:unsubscribe()
+        end)
+    end
+end
+
+function drawGame()
+    x = screenw/2-(wspacing*3)/2+wspacing
+    y = 50
+    for i=1,2 do
+        love.graphics.setColor(1,1,1)
+        love.graphics.rectangle('fill',x,y,10,hspacing*3)
+        x=x+wspacing
+    end
+    x = screenw/2-(wspacing*3)/2
+    y = 50+hspacing
+    for i=1,2 do
+        love.graphics.setColor(1,1,1)
+        love.graphics.rectangle('fill',x,y,wspacing*3,10)
+        y=y+hspacing
+    end
+    for i=1,9 do --search for any obj
+        local obj = board[i]
+        bx = (i-1)%3+1 --block x
+        by = math.floor((i-1)/3)+1 --block y
+        if obj==1 then
+            local size = 25
+            x = screenw/2-(wspacing*3)/2+(wspacing*(bx-1))+(wspacing/2-size/2)+5
+            y = 50+(hspacing*(by-1))+(hspacing/2-size/2)+5
+            love.graphics.setColor(redcolors[1])
+            love.graphics.rectangle('fill',x,y,size,size)
+        elseif obj==2 then
+            local size = 25
+            x = screenw/2-(wspacing*3)/2+(wspacing*(bx-1))+(wspacing/2-size/2)+5
+            y = 50+(hspacing*(by-1))+(hspacing/2-size/2)+5
+            love.graphics.setColor(bluecolors[1])
+            love.graphics.rectangle('fill',x,y,size,size)
+        elseif obj==3 then
+            local size = 50
+            x = screenw/2-(wspacing*3)/2+(wspacing*(bx-1))+(wspacing/2-size/2)+5
+            y = 50+(hspacing*(by-1))+(hspacing/2-size/2)+5
+            love.graphics.setColor(redcolors[2])
+            love.graphics.rectangle('fill',x,y,size,size)
+        elseif obj==4 then
+            local size = 50
+            x = screenw/2-(wspacing*3)/2+(wspacing*(bx-1))+(wspacing/2-size/2)+5
+            y = 50+(hspacing*(by-1))+(hspacing/2-size/2)+5
+            love.graphics.setColor(bluecolors[2])
+            love.graphics.rectangle('fill',x,y,size,size)
+        elseif obj==5 then
+            local size = 75
+            x = screenw/2-(wspacing*3)/2+(wspacing*(bx-1))+(wspacing/2-size/2)+5
+            y = 50+(hspacing*(by-1))+(hspacing/2-size/2)+5
+            love.graphics.setColor(redcolors[3])
+            love.graphics.rectangle('fill',x,y,size,size)
+        elseif obj==6 then
+            local size = 75
+            x = screenw/2-(wspacing*3)/2+(wspacing*(bx-1))+(wspacing/2-size/2)+5
+            y = 50+(hspacing*(by-1))+(hspacing/2-size/2)+5
+            love.graphics.setColor(bluecolors[3])
+            love.graphics.rectangle('fill',x,y,size,size)
+        end
+    end
+    x=25
+    y=screenh-screenh/4+25
+    for i=1,3 do --draw squares
+        local size = 25*i
+        local spacing = 25*i+10
+        if team==1 then
+            love.graphics.setColor(redcolors[i])
+        elseif team==2 then
+            love.graphics.setColor(bluecolors[i])
+        end
+        love.graphics.rectangle('fill',x+(spacing/2-size/2),y+(75/2-size/2),size,size)
+        love.graphics.setColor(1,1,1)
+        if selectedSquare==i then
+            love.graphics.rectangle('line',x+(spacing/2-size/2),y+(75/2-size/2),size,size,5)
+        end
+        love.graphics.printf(squaresAvailable[i],font,x,y+75,spacing,"center")
+        x=x+spacing
+    end
+    love.graphics.setColor(1,1,1)
+    if myTurn then
+        love.graphics.printf("É seu turno!",font,0,y+15,screenw-screenw/15,"right")
+    else
+        love.graphics.printf("Aguarde!",font,0,y+15,screenw-screenw/15,"right")
+    end
+end
+
+function drawEndMenu()
+    y = 50
+    love.graphics.printf("GigaVelha",font,0,y,screenw,"center")
+    y=y+font:getHeight()*2.5
+    love.graphics.printf(gameMessage,font,15,y,screenw-30,"center")
+    local tw,th = font:getWrap(gameMessage,screenw-30)
+    y=y+font:getHeight()*#th+30
+    if menu=="end" then
+        btnWrap("Revanche",y,function ()
+            publish("rematch","")
+            menu="rematchask"
+        end)
+    elseif menu=="rematch" then
+        local tw,th = font:getWrap("O oponente propos uma revanche",screenw-30)
+        love.graphics.printf("O oponente propos uma revanche",font,15,y,screenw-30,"center")
+        y=y+font:getHeight()*#th+30
+        btnWrap("Aceitar!",y,function ()
+            publish("rematchaccept","")
+            resetVars()
+            menu="game"
+        end)
+    elseif menu=="rematchask" then
+        love.graphics.printf("Revanche pedida",font,15,y,screenw-30,"center")
+    end
+    y=screenh-font:getHeight()-45
+    btnWrap("Voltar",y,function ()
+        menu="start"
+        hub:unsubscribe()
+    end)
+end
+
+function drawConfig()
+    
+end
+
+--draws a button centralized in the desired y pos
+--text, y pos, function
+function btnWrap(text,y,fun)
+    local tw,th = font:getWrap(text,screenw-30)
+    button(text,screenw/2-tw/2-15,y,tw+30,fun)
+end
+
+
+--draw a button
+--text, x pos, y pos, width, function
+function button(text,x,y,w,fun)
+    local tw,th = font:getWrap(text,w-30)
+    local height = font:getHeight()*#th+30
+
+    local color = {0.45,0.45,0.45}
+
+    if love.mouse.isDown(1) then
+        local mx,my = love.mouse.getPosition()
+        if mx >= x and mx <= x+w and my >= y and my <= y+height then
+            unableFn=fun
+            color={0.55,0.55,0.55}
+            unable=true
+        end
+    end
+
+    love.graphics.setColor(0.1,0.1,0.1)
+    love.graphics.rectangle('fill',x+10,y+10-15,w,height)
+    
+    love.graphics.setColor(color)
+    love.graphics.rectangle('fill',x,y-15,w,height)
+    
+    love.graphics.setColor(1,1,1)
+    love.graphics.printf(text,font,x+15,y,w-30,"center")
+end
+
+function textboxWrapper(label,text,y)
+    local w = screenw-15-15
+    love.graphics.setColor(1,1,1)
+    love.graphics.printf(label,font,15,y,w,"left")
+    local tw = font:getWidth(label)+15
+    textbox(text,15+tw,y,w-tw)
+end
+
+function textbox(text,x,y,w)
+    local height = font:getHeight()+30
+
+    love.graphics.setColor(0.1,0.1,0.1)
+    love.graphics.rectangle('fill',x+5,y+5-15,w,height)
+    
+    love.graphics.setColor(0.45,0.45,0.45)
+    love.graphics.rectangle('fill',x,y-15,w,height)
+
+    love.graphics.setColor(0.8,0.8,0.8)
+    love.graphics.rectangle('fill',x+5,y+height-5-15-5,w-10,5)
+
+    love.graphics.setColor(1,1,1)
+    love.graphics.printf(text,font,x+15,y,w-30,"left")
+
+    if love.keyboard.hasTextInput() and text~="" and cursorBlinkNow then
+        local tw = font:getWidth(text)
+        love.graphics.rectangle('fill',x+15+tw,y,5,font:getHeight())
+    end
+
+    if love.mouse.isDown(1) then
+        local mx,my = love.mouse.getPosition()
+        if mx >= x and mx <= x+w and my >= y and my <= y+height then
+            love.keyboard.setTextInput(true)
+        end
+    end
+end
+
+function drawBoard(ny)
+    x = screenw/2-(wspacing*3)/2+wspacing
+    y = ny
+    for i=1,2 do
+        love.graphics.setColor(1,1,1)
+        love.graphics.rectangle('fill',x,y,10,hspacing*3)
+        x=x+wspacing
+    end
+    x = screenw/2-(wspacing*3)/2
+    y = ny+hspacing
+    for i=1,2 do
+        love.graphics.setColor(1,1,1)
+        love.graphics.rectangle('fill',x,y,wspacing*3,10)
+        y=y+hspacing
+    end
+    for i=1,9 do --search for any obj
+        local obj = dummyBoard[i]
+        bx = (i-1)%3+1 --block x
+        by = math.floor((i-1)/3)+1 --block y
+        if obj==1 then
+            local size = 25
+            x = screenw/2-(wspacing*3)/2+(wspacing*(bx-1))+(wspacing/2-size/2)+5
+            y = ny+(hspacing*(by-1))+(hspacing/2-size/2)+5
+            love.graphics.setColor(redcolors[1])
+            love.graphics.rectangle('fill',x,y,size,size)
+        elseif obj==2 then
+            local size = 25
+            x = screenw/2-(wspacing*3)/2+(wspacing*(bx-1))+(wspacing/2-size/2)+5
+            y = ny+(hspacing*(by-1))+(hspacing/2-size/2)+5
+            love.graphics.setColor(bluecolors[1])
+            love.graphics.rectangle('fill',x,y,size,size)
+        elseif obj==3 then
+            local size = 50
+            x = screenw/2-(wspacing*3)/2+(wspacing*(bx-1))+(wspacing/2-size/2)+5
+            y = ny+(hspacing*(by-1))+(hspacing/2-size/2)+5
+            love.graphics.setColor(redcolors[2])
+            love.graphics.rectangle('fill',x,y,size,size)
+        elseif obj==4 then
+            local size = 50
+            x = screenw/2-(wspacing*3)/2+(wspacing*(bx-1))+(wspacing/2-size/2)+5
+            y = ny+(hspacing*(by-1))+(hspacing/2-size/2)+5
+            love.graphics.setColor(bluecolors[2])
+            love.graphics.rectangle('fill',x,y,size,size)
+        elseif obj==5 then
+            local size = 75
+            x = screenw/2-(wspacing*3)/2+(wspacing*(bx-1))+(wspacing/2-size/2)+5
+            y = ny+(hspacing*(by-1))+(hspacing/2-size/2)+5
+            love.graphics.setColor(redcolors[3])
+            love.graphics.rectangle('fill',x,y,size,size)
+        elseif obj==6 then
+            local size = 75
+            x = screenw/2-(wspacing*3)/2+(wspacing*(bx-1))+(wspacing/2-size/2)+5
+            y = ny+(hspacing*(by-1))+(hspacing/2-size/2)+5
+            love.graphics.setColor(bluecolors[3])
+            love.graphics.rectangle('fill',x,y,size,size)
+        end
+    end
+end
+
+function newDummyBoard()
+    if #dummyBoard==0 then
+        for i=1,9 do
+            dummyBoard[i] = math.random(0,6)
+        end
+    end
+end
+
+function resetDummyBoard()
+    for i=1,9 do
+        dummyBoard[i] = math.random(0,6)
+    end
+end
